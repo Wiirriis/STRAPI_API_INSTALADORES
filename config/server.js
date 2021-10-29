3d60fe1eb560ee8a2955d5f68c6c5274be48fe81
Cesar <cpolcam@outlook.com>
2021-10-29 13:44:35 -0600

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9fbe53b6606158ac9140b14a4f328591'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '329c30b9-0cc4-4b52-be1d-4fcfd5cfdc23'
+};
